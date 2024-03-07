6d88c84f038a2e859679c30ce15069c1586e63ce
Luis Olivetti <luuisf92@gmail.com>
2024-03-07 12:38:59 +0000

    Add print report test case to stress_test.go

--- internal/stress_test.go
+++ internal/stress_test.go
@@ -1,10 +1,13 @@
 package internal
 
 import (
+	"fmt"
+	"os"
 	"sync"
 	"testing"
 
 	"github.com/gin-gonic/gin"
+	"github.com/luis-olivetti/go-stresstest/internal/model"
 )
 
 var (
@@ -13,6 +16,7 @@ var (
 )
 
 func TestStress(t *testing.T) {
+	// Arrange
 	urlStr := "http://localhost:8080"
 	concurrency := 2
 	requests := 4
@@ -22,8 +26,10 @@ func TestStress(t *testing.T) {
 	go server.Run(":8080")
 
 	stress := NewStress(urlStr, concurrency, requests)
+	// Act
 	report := stress.Run()
 
+	// Assert
 	if report.TotalRequests != requests {
 		t.Errorf("Expected %d, got %d", requests, report.TotalRequests)
 	}
@@ -42,6 +48,58 @@ func TestStress(t *testing.T) {
 
 	if report.HTTPCodes[500] != 1 {
 		t.Errorf("Expected 1, got %d", report.HTTPCodes[500])
+	}
+}
+
+func TestPrintReport(t *testing.T) {
+	// Arrange
+	urlStr := "http://localhost:8080"
+	concurrency := 2
+	requests := 100
+
+	stress := NewStress(urlStr, concurrency, requests)
+
+	report := &model.Report{
+		TotalTime:     10,
+		TotalRequests: 100,
+		HTTPCodes: map[int]int{
+			200: 50,
+			404: 20,
+			500: 30,
+		},
+	}
+
+	file, err := os.CreateTemp("", "output.txt")
+	if err != nil {
+		t.Fatalf("Failed to create temporary file: %s", err)
+	}
+	defer os.Remove(file.Name())
+	defer file.Close()
+
+	old := os.Stdout
+	os.Stdout = file
+	defer func() {
+		os.Stdout = old
+	}()
+
+	// Act
+	stress.PrintReport(report)
+
+	// Assert
+	expectedOutput := fmt.Sprintf("\nReport:\n\n> Total time: %s\n\n> Total requests: %d\n\n> HTTP Code %d: %d req(s)\n> HTTP Code %d: %d req(s)\n> HTTP Code %d: %d req(s)\n",
+		report.TotalTime, report.TotalRequests,
+		200, report.HTTPCodes[200],
+		404, report.HTTPCodes[404],
+		500, report.HTTPCodes[500])
+
+	fileContent, err := os.ReadFile(file.Name())
+	if err != nil {
+		t.Errorf("Failed to read file: %s", err)
+		return
+	}
+
+	if string(fileContent) != expectedOutput {
+		t.Errorf("PrintReport output is incorrect, got: %s, want: %s", string(fileContent), expectedOutput)
 	}
 }
 
